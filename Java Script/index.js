301b58b2d786d6c2d8b2be6074ad565d38f12cbd
gabby8484 <113048995+gabby8484@users.noreply.github.com>
2023-03-17 18:03:57 -0500

    HW 19!

---
+++ Java Script/index.js
@@ -0,0 +1,17 @@
+//create a variable below
+var HW19=("this is the variable value");
+//console.log() your variable here.
+console.log("index.js is Correctly loaded!");
+
+//use jQuery to target the <h1> tag
+$("h1").css("color","yellow");
+
+$("button").hover(function(){
+    $(this).css("background-color","#00FF00");
+  },
+  function(){
+    $(this).css("background-color", "#00FF00");
+  });
+
+
+
